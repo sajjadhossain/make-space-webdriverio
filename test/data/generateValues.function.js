e4ae9891e94e24cdb7c9bb87e47c474cbb580836
Sajjad Hossain <sajjad@withpulp.com>
2019-08-09 18:22:18 -0400

    added video

--- test/data/generateValues.function.js
+++ test/data/generateValues.function.js
@@ -1,10 +1,19 @@
+const makePhoneNumber = () => {
+   var result           = ''
+   var characters       = '0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < 7; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+   }
+   return '1347' + result
+}
 const generateValues = () => {
   let randomName
   let randomPhoneNumber
   let randomEmail
 
   randomName = Math.random().toString(36).substring(7) + ' ' + Math.random().toString(36).substring(9)
-  randomPhoneNumber = '1' + Math.floor(Math.random() * 9000000000)
+  randomPhoneNumber = makePhoneNumber()
   randomEmail = Math.random().toString(36).substring(7) + '@' + Math.random().toString(36).substring(7) + '.com'
 
   return {
